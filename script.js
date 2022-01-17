a442ede0bf5732917f5e65543883d7994b390387
rameshkumar359 <rameshkumar359.k@gmail.com>
2022-01-17 21:18:25 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+let time=10
+let heading=document.querySelector(".heading")
+let animation=document.querySelector(".heading1")
+let g=" "
+
+heading.innerHTML=time
+setTimeout(()=>{
+    heading.innerHTML=time-1
+    setTimeout(()=>{
+        heading.innerHTML=time-2
+       setTimeout(()=>{
+           heading.innerHTML=time-3
+           setTimeout(()=>{
+               heading.innerHTML=time-4
+               setTimeout(()=>{
+                   heading.innerHTML=time-5
+                   setTimeout(()=>{
+                       heading.innerHTML=time-6
+                       setTimeout(()=>{
+                           heading.innerHTML=time-7
+                           setTimeout(()=>{
+                               heading.innerHTML=time-8
+                               setTimeout(()=>{
+                                   heading.innerHTML=time-9
+                                       setTimeout(()=>{
+                                           animation.innerHTML="HAPPY REPUBLIC DAY!"
+                                           heading.innerHTML=g
+                                       },1000)
+                               },1000)
+                           },1000)
+                       },1000)
+                   },1000)
+               },1000)
+           },1000)
+       },1000)
+    },1000)
+},1000)
